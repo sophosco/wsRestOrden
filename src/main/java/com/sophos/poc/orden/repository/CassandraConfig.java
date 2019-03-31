57fe22ff7f324fc302fbb44e72a1d7cda07089b3
Juan Diego <juandiegosophos@gmail.com>
2019-03-31 15:11:51 -0500

    Ajustes en variables de entorno Cassandra AB#1314

--- src/main/java/com/sophos/poc/orden/repository/CassandraConfig.java
+++ src/main/java/com/sophos/poc/orden/repository/CassandraConfig.java
@@ -1,49 +1,61 @@
 package com.sophos.poc.orden.repository;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.CassandraSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.core.CassandraOperations;
-import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.convert.CassandraConverter;
 import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
 import org.springframework.data.cassandra.core.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SuppressWarnings("deprecation")
 @Configuration
-@EnableCassandraRepositories("com.sophos.poc.orden.repository")
-public class CassandraConfig extends AbstractCassandraConfiguration{
+@EnableCassandraRepositories("com.sophos.poc.login.repository")
+public class CassandraConfig extends AbstractCassandraConfiguration {
 
-    private static final String KEYSPACE_NAME = System.getenv("POC_CASSANDRA_KEYSPACE");
-    private static final String CONTACT_POINTS = System.getenv("POC_CASSANDRA_HOST");
-    private static final int PORT = Integer.parseInt(
-    			System.getenv("POC_CASSANDRA_PORT")!=null && !System.getenv("POC_CASSANDRA_PORT").isEmpty()?
-    					System.getenv("POC_CASSANDRA_PORT"):
-    					"9042"
-    			);
+	private static final String USERNAME = System.getenv("POC_CASSANDRA_USERNAME");
+	private static final String PASSWORD = System.getenv("POC_CASSANDRA_PASSWORD");
+	private static final String KEYSPACE_NAME = System.getenv("POC_CASSANDRA_KEYSPACE");
+	private static final String CONTACT_POINTS = System.getenv("POC_CASSANDRA_HOST");
+	private static final int PORT = Integer.parseInt(System.getenv("POC_CASSANDRA_PORT"));
 
-    @Override
-    protected String getKeyspaceName() {
-        return KEYSPACE_NAME;
-    }
+		@Bean
+	    public CassandraClusterFactoryBean cluster() {
+	        CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+	        cluster.setUsername(USERNAME);
+	        cluster.setPassword(PASSWORD);
+	        cluster.setContactPoints(CONTACT_POINTS);
+	        cluster.setPort(PORT);
+	        return cluster;
+	    }
+	    @Bean
+	    public CassandraMappingContext mappingContext() {
+	        return new BasicCassandraMappingContext();
+	    }
+	    
+	    @Bean
+	    public CassandraConverter converter() {
+	        return new MappingCassandraConverter(mappingContext());
+	    }
+	    
+	    @Bean
+	    public CassandraSessionFactoryBean session() {
+	        CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
+	        session.setCluster(cluster().getObject());
+	        session.setKeyspaceName(KEYSPACE_NAME);
+	        session.setConverter(converter());
+	        session.setSchemaAction(SchemaAction.NONE);
+	        return session;
+	    }
+	    
+	    
+		@Override
+		protected String getKeyspaceName() {
+			return KEYSPACE_NAME;
+		}
 
-    @Override
-    protected String getContactPoints() {
-        return CONTACT_POINTS;
-    }
-
-    @Override
-    protected int getPort() {
-        return PORT;
-    }
-
-    @Override
-    public SchemaAction getSchemaAction() {
-        return SchemaAction.NONE;
-    }
-
-	@Bean
-    public CassandraOperations operations() throws Exception {
-        return new CassandraTemplate(session().getObject(), new MappingCassandraConverter(new BasicCassandraMappingContext()));
-    }
 }
